0a2f0e680f5f12c40ba746fd16a9360478af2f2d
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-04-21 17:32:00 +0500

    Added Image fields in projects model and migrated. Also edited the projects in the table

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    image = models.FileField(upload_to='projects_images/', blank=True)
 
     def __str__(self):
         return self.title
