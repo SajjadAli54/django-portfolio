215d7aaf18a9c9cb7c30a9e1a1c74aa12ac2a05d
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-04-21 16:53:32 +0500

    Developed base line and installed new projects App

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "projects.apps.ProjectsConfig",
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'personal_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
