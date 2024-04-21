e207322f8c1140d748233059ceccd011a5febfcb
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-04-21 17:15:59 +0500

    added projects view functionality and migrated other migrations as well

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from projects.models import Project
+
 # Create your views here.
+
+def project_index(request: HttpRequest) -> HttpResponse:
+    projects = Project.objects.all()
+    return render(request, 'projects/project_index.html', {'projects': projects})
+
+def project_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    project = Project.objects.filter(pk=pk).first()
+    return render(request, 'projects/project_detail.html', {'project': project})
